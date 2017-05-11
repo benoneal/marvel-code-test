36ab5c56ac6142e983e47ae404a54d4e9fcd12b5
Ben O'Neal <ben@benoneal.com>
2017-05-11 23:05:15 +1000

    Prepare for heroku

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const PRODUCTION = process.env.NODE_ENV === 'production'
 const MARVEL_API_URL = 'https://gateway.marvel.com:443/v1/public/'
 
 module.exports = {
-  app_url: PRODUCTION ? 'http://localhost:3000/' : 'http://localhost:3000/',
+  app_url: PRODUCTION ? 'https://sixdegreesofironman.herokuapp.com/' : 'http://localhost:3000/',
   api_base_url: MARVEL_API_URL,
   targetCharURI: MARVEL_API_URL + 'characters/1009368',
   startingCharURIs: [
